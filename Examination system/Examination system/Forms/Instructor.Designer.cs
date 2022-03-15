fd84472c9c4a697bb592fbdb858db5bf95b7d7c7
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-03-15 21:18:13 +0200

    test

--- Examination system/Examination system/Forms/Instructor.Designer.cs
+++ Examination system/Examination system/Forms/Instructor.Designer.cs
@@ -29,6 +29,8 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -40,20 +42,44 @@
 			this.button1.Text = "button1";
 			this.button1.UseVisualStyleBackColor = true;
 			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(476, 86);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(85, 17);
+			this.radioButton1.TabIndex = 1;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(513, 279);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
 			// Instructor
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.radioButton1);
 			this.Controls.Add(this.button1);
 			this.Name = "Instructor";
 			this.Text = "Instructor";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.ComboBox comboBox1;
 	}
 }
